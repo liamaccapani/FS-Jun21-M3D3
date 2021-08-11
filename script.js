ac048fbba449441979e0b9c165ac2e8040b98cbc
liamaccapani <lia.maccapani@gmail.com>
2021-08-11 21:56:18 +0200

    Managed to display pics
    Still need to be completed + refactor (not dry)

--- script.js
+++ script.js
@@ -1,57 +1,105 @@
 //563492ad6f91700001000001ac80369c68314994b36da40f5a0e98b0
-const getPictures = function () {
-    fetch("https://api.pexels.com/v1/search?query=people", {
-        "method": "GET",
-        "headers": {Authorization: '563492ad6f91700001000001ac80369c68314994b36da40f5a0e98b0'}
 
+const getPhotos = function () {
+  fetch("https://api.pexels.com/v1/search?query=dog", {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer 563492ad6f91700001000001ac80369c68314994b36da40f5a0e98b0",
+    },
+  })
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+      displayPics(jsonResponse); //✨✨✨✨
     })
-    .then((response) => {
-        console.log(response, 'hey yo!') 
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
-        return response.json() // ✨ return ✨
+//and separate functions to display things based on the button I click
+const displayPics = function (jsonResponse) { //✨✨✨✨
+  let row = document.querySelector("#album-container");
+  for (let photo of jsonResponse.photos) {
+    row.innerHTML += `
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+                <img src=${photo.src.landscape} alt="some pic">
 
-    })
-    .then(jsonResponse=> {
-        console.log(jsonResponse)
-        
-        let parentDiv = document.querySelector(".container .row")
-        for (let photos of jsonResponse.photos){ 
-            parentDiv.innerHTML += `
-            
-            <div class="card mb-4 shadow-sm">       
-              <img src=${photos.src.landscape}>
-              <div class="card-body">
-                <p class="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <div
-                  class="d-flex justify-content-between align-items-center"
-                >
-                  <div class="btn-group">
-                    <button
-                      type="button"
-                      class="btn btn-sm btn-outline-secondary"
-                    >
-                      View
-                    </button>
-                    <button
-                      type="button"
-                      class="btn btn-sm btn-outline-secondary"
-                    >
-                      Edit
-                    </button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
+                <div class="card-body">
+                    <p class="card-text">
+                        This is a wider card with supporting text below as a natural
+                        lead-in to additional content. This content is a little bit
+                        longer.
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">
+                            View
+                        </button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">
+                            Edit
+                        </button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
                 </div>
-              </div>
             </div>
-          
-            `
-            console.log(photos.src.landscape)
-        }
+        </div> <!--div col-->
+        `;
+  }
+};
+
+const getPhotos2 = function () {
+    fetch("https://api.pexels.com/v1/search?query=colorful", {
+      method: "GET",
+      headers: {
+        Authorization:
+          "Bearer 563492ad6f91700001000001ac80369c68314994b36da40f5a0e98b0",
+      },
     })
-} 
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        displayPics2(jsonResponse);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
+  const displayPics2 = function (jsonResponse) {
 
+    let row = document.querySelector("#album-container");
+    for (let photo of jsonResponse.photos) {
+      row.innerHTML += `
+          <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                  <img src=${photo.src.landscape} alt="some pic">
+  
+                  <div class="card-body">
+                      <p class="card-text">
+                          This is a wider card with supporting text below as a natural
+                          lead-in to additional content. This content is a little bit
+                          longer.
+                      </p>
+                      <div class="d-flex justify-content-between align-items-center">
+                          <div class="btn-group">
+                          <button type="button" class="btn btn-sm btn-outline-secondary">
+                              View
+                          </button>
+                          <button type="button" class="btn btn-sm btn-outline-secondary">
+                              Edit
+                          </button>
+                          </div>
+                          <small class="text-muted">9 mins</small>
+                      </div>
+                  </div>
+              </div>
+          </div> <!--div col-->
+          `;
+    }
+  };
+
+  //remove/ hide pics of getphotos()
+
+//anyway, again, neat code, and also thank you, now i can load some cute random doggo pics instead of go search for them on Instagram 
