932dc2ac6a5432fa68bc746824deb7030d6f6b9c
liamaccapani <lia.maccapani@gmail.com>
2021-08-11 17:07:33 +0200

    Whaaat?

--- script.js
+++ script.js
@@ -13,12 +13,45 @@ const getPictures = function () {
     })
     .then(jsonResponse=> {
         console.log(jsonResponse)
-        const parentDiv = document.querySelector(".container .row")
-for (let photos of jsonResponse.photos){ 
-    console.log(parentDiv)
-    console.log(photos)
-}
-})
+        
+        let parentDiv = document.querySelector(".container .row")
+        for (let photos of jsonResponse.photos){ 
+            parentDiv.innerHTML += `
+            
+            <div class="card mb-4 shadow-sm">       
+              <img src=${photos.src.landscape}>
+              <div class="card-body">
+                <p class="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <div
+                  class="d-flex justify-content-between align-items-center"
+                >
+                  <div class="btn-group">
+                    <button
+                      type="button"
+                      class="btn btn-sm btn-outline-secondary"
+                    >
+                      View
+                    </button>
+                    <button
+                      type="button"
+                      class="btn btn-sm btn-outline-secondary"
+                    >
+                      Edit
+                    </button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          
+            `
+            console.log(photos.src.landscape)
+        }
+    })
 } 
 
 
